e7dcd85448c83dce614183201e1374599c8be108
galeswkwkw <kristiawan7612@gmail.com>
2026-08-19 10:50:39 +0700

    get db

--- app/database.py
+++ app/database.py
@@ -6,3 +6,11 @@ from app.config import Config
 engine = create_engine(Config.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    """Dependency database session"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
